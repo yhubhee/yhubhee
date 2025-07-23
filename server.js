acbcf6fe0b4f176b3808699f8e9ce57ab4b9c25d
yhubhee <emenyiubongabasi415@gmail.com>
2025-07-23 15:57:23 +0100

    updated commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.set('views', path.join(__dirname, 'views'));
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res)=>{
